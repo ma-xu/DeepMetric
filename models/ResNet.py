dbeb8949f4a70fd761c53dd1e81fa9bda89c9e27
13952522076 <xuma@my.unt.edu>
2020-06-16 19:26:25 -0500

    update

--- models/ResNet.py
+++ models/ResNet.py
@@ -117,7 +117,7 @@ class Bottleneck(nn.Module):
 
 class ResNet(nn.Module):
 
-    def __init__(self, block, layers, num_classes=1000, zero_init_residual=False,
+    def __init__(self, block, layers, dim=1000, zero_init_residual=False,
                  groups=1, width_per_group=64, replace_stride_with_dilation=None,
                  norm_layer=None):
         super(ResNet, self).__init__()
@@ -149,7 +149,7 @@ class ResNet(nn.Module):
         self.layer4 = self._make_layer(block, 512, layers[3], stride=2,
                                        dilate=replace_stride_with_dilation[2])
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = nn.Linear(512 * block.expansion, num_classes)
+        self.fc = nn.Linear(512 * block.expansion, dim)
 
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
